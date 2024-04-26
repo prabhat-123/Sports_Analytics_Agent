86552b4c4e0d32e4dea52f3a765a3962d6d3e851
prabhat-123 <srv.ale52@gmail.com>
2024-04-27 00:44:56 +0545

    ipl agent with very basic functionality on ipl matches 2008 to 2012

---
+++ IPL_Agent/src/db_access.py
@@ -0,0 +1,42 @@
+import sqlalchemy as sa
+import pandas as pd
+from config.constants import (
+    POSTGRES_DB,
+    POSTGRES_USER,
+    POSTGRES_HOST,
+    POSTGRES_PORT,
+    CSV_DATA_PATH,
+    POSTGRES_TABLE,
+    POSTGRES_PASSWORD
+)
+
+class PostgresDB:
+    def __init__(self):
+        self.csv_path = CSV_DATA_PATH
+        self.db_name = POSTGRES_DB
+        self.db_user = POSTGRES_USER
+        self.db_password = POSTGRES_PASSWORD
+        self.db_host = POSTGRES_HOST
+        self.db_port = POSTGRES_PORT
+        self.table = POSTGRES_TABLE
+        self.engine = None
+
+    def create_connection_string(self):
+        return f"postgresql://{self.db_user}:{self.db_password}@{self.db_host}:{self.db_port}/{self.db_name}"
+
+    def connect_to_db(self):
+        connection_string = self.create_connection_string()
+        self.engine = sa.create_engine(connection_string)
+
+    def insert_data_into_db(self):
+        if self.engine is None:
+            raise Exception("Database connection not initialized. Call connect_to_db() first.")
+        
+        df = pd.read_csv(self.csv_path)
+        df.to_sql(self.table, self.engine, schema="", if_exists='replace')
+
+
+if __name__ == "__main__":
+    db = PostgresDB()
+    db.connect_to_db()
+    db.insert_data_into_db()
---
+++ IPL_Agent/src/sql_agent.py
@@ -0,0 +1,25 @@
+import os
+from config.constants import MODEL_NAME
+from config.prompt import generate_final_prompt
+from langchain_community.utilities import SQLDatabase
+from langchain_community.agent_toolkits import create_sql_agent
+from langchain_openai import ChatOpenAI
+from db_access import PostgresDB
+
+db = PostgresDB()
+connection_string = db.create_connection_string()
+
+db = SQLDatabase.from_uri(connection_string)
+print(db.dialect)
+print(db.get_usable_table_names())
+final_prompt = generate_final_prompt()
+llm = ChatOpenAI(model= MODEL_NAME, temperature = 0)
+agent_executor = create_sql_agent(llm, db=db, 
+                                  agent_type="openai-tools", 
+                                  verbose=True, prompt=final_prompt)
+query_input = input("Please ask question related to IPL\n")
+response = agent_executor.invoke({"input": query_input})
+print(response)
+
+
+
